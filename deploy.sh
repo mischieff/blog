b37c1113418b5c213cf11c73faed3ffe38c4de11
mischieff <huangchaoezu@sina.com>
2021-01-12 17:58:30 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.iyarn docs:devo
-git push -f git@github.com:mischieff/blog.github.io.git master
+# git push -f git@github.com:mischieff/blog.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:mischieff/blog.git master:gh-pages
+git push -f git@github.com:mischieff/blog.git master:gh-pages
 
 cd -
 
